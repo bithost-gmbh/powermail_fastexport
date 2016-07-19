eb7fb59b010d42bc8606fe8dbd801f998c10d1dd
Esteban Marín <esteban.marin@bithost.ch>
2016-07-19 07:17:05 +0200

    BKWCH-55: remove unused code

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,3 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function($extKey)
-    {
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'Powermail FastExport');
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_powermailfastexport_domain_model_module', 'EXT:powermail_fastexport/Resources/Private/Language/locallang_csh_tx_powermailfastexport_domain_model_module.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_powermailfastexport_domain_model_module');
-
-    },
-    $_EXTKEY
-);
